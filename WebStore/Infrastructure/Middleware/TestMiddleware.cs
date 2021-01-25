4c01a7ec37142c911efd339c30ce12068b8883f5
IVfromBY <ivfromby@gmail.com>
2021-01-25 21:30:52 +0300

    промеджуточное ПО

---
+++ WebStore/Infrastructure/Middleware/TestMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Infrastructure.Middleware
+{
+    public class TestMiddleware
+    {
+        private readonly RequestDelegate _Next;
+
+        public TestMiddleware(RequestDelegate Next) => _Next = Next;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Дейтвие до следующего узла в контейнере
+            //context.Request
+            //context.Response - если изменяем ответ, то прерываем ответ
+            var next = _Next(context);
+            
+            // дейстиве во время того, как оставшеаяся часть конвейер что-то делате
+            
+            await next; //Точка синхронизации
+
+            // Действие после завершения работы осавшейся части  конвейера
+        }
+    }
+}
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebStore.Infrastructure.Middleware;
 
 namespace WebStore
 {
@@ -26,7 +27,18 @@ namespace WebStore
             app.UseStaticFiles();
 
             app.UseRouting();
-            
+
+            app.UseWelcomePage("/wel");
+
+            app.UseMiddleware<TestMiddleware>();
+
+            app.MapWhen(
+                 context => context.Request.Query.ContainsKey("id") && context.Request.Query["id"]=="5",
+                 context => context.Run(async request => await request.Response.WriteAsync("Hell id = 5!!!"))
+                );
+
+            app.Map("/hhh", context => context.Run(async request => await request.Response.WriteAsync("Hell!!!")));
+
             app.UseEndpoints(endpoints =>
             {
                 //Проекция запроса на действие
