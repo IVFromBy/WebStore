d8a45274618a1e279b1f0bd9327b681e7d967b41
IVfromBY <ivfromby@gmail.com>
2021-02-03 23:02:43 +0300

    Заготовка контроллера акаунта

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entites.Identity;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _UserManager;
+        private readonly RoleManager<Role> _RoleManager;
+
+        public AccountController(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _UserManager = userManager;
+            _RoleManager = roleManager;
+        }
+    }
+}
