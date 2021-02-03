28655d6ccebf49642ba0f54ebaa1f6b5b9f49059
IVfromBY <ivfromby@gmail.com>
2021-02-03 23:34:11 +0300

    Регистрация пользователя

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -5,18 +5,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Domain.Entites.Identity;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
         private readonly UserManager<User> _UserManager;
-        private readonly RoleManager<Role> _RoleManager;
+        private readonly SignInManager<User> _SignInManager;
 
-        public AccountController(UserManager<User> userManager, RoleManager<Role> roleManager)
+        public AccountController(UserManager<User> userManager, SignInManager<User> SignInManager)
         {
             _UserManager = userManager;
-            _RoleManager = roleManager;
+            _SignInManager = SignInManager;
         }
+
+        public IActionResult Register() => View(new RegisterViewModel());
+        
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName,
+            };
+
+            var registration_result = await _UserManager.CreateAsync(user, Model.Password);
+
+            if(registration_result.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registration_result.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(Model);
+        }
+
     }
 }
