f7c9daeab2e72df63b140277a1ebf03e6877e7f3
IVfromBY <ivfromby@gmail.com>
2021-02-14 13:39:34 +0300

    Профиль пользователя

---
+++ WebStore/Controllers/UserProfileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> Orders([FromServices] IOrderService orderService)
+        {
+            var orders = await orderService.GetUserOrders(User.Identity.Name);
+
+            return View(orders.Select(o => new UserOrderViewModel
+            {
+                Id = o.Id,
+                Name = o.Name,
+                Address = o.Address,
+                Phone = o.Phone,
+                TotalPrice = o.Items.Sum(i => i.Quantty * i.Price),
+
+            }
+
+                ));
+        }
+    }
+}
