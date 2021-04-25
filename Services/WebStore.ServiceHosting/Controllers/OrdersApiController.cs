616dbf8f266223a1c5a9702bd123f982b891faea
IVfromBY <ivfromby@gmail.com>
2021-04-25 20:05:59 +0300

    Документация к WebAPI

--- Services/WebStore.ServiceHosting/Controllers/OrdersApiController.cs
+++ Services/WebStore.ServiceHosting/Controllers/OrdersApiController.cs
@@ -9,7 +9,9 @@ using WebStore.Infrastructure.Interfaces;
 using WebStore.Interfaces;
 
 namespace WebStore.ServiceHosting.Controllers
-{
+{/// <summary>
+/// Управление заказами
+/// </summary>
     [Route(WebAPI.Orders)]
     [ApiController]
     public class OrdersApiController : ControllerBase, IOrderService
@@ -17,11 +19,26 @@ namespace WebStore.ServiceHosting.Controllers
         private readonly IOrderService _orderService;
 
         public OrdersApiController(IOrderService orderService) => _orderService = orderService;
-
+        /// <summary>
+        /// Создание ноовго заказа
+        /// </summary>
+        /// <param name="UserName">Имя пользователя-заказчика</param>
+        /// <param name="OrderModel">Информация о заказе</param>
+        /// <returns>Информация о сформированном заказе</returns>
         [HttpPost("{UserName}")]
         public Task<OrderDto> CreateOrder(string UserName,[FromBody] CreateOrderModel OrderModel) => _orderService.CreateOrder(UserName, OrderModel);
+        /// <summary>
+        /// Получение заказа по его диентификатору
+        /// </summary>
+        /// <param name="id">Id заказа</param>
+        /// <returns>Информация о заказе</returns>
         [HttpGet("{id}")]
         public Task<OrderDto> GetOrderById(int id) => _orderService.GetOrderById(id);
+        /// <summary>
+        /// Получение всех заказов пользователя
+        /// </summary>
+        /// <param name="UserName">Имя пользователя</param>
+        /// <returns>Перечень заказов пользователя</returns>
         [HttpGet("user/{UserName}")]
         public Task<IEnumerable<OrderDto>> GetUserOrders(string UserName) => _orderService.GetUserOrders(UserName);
     }
