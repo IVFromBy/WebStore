2c029534a10ba2ae70b9ada6f20560c74739f5ff
IVfromBY <ivfromby@gmail.com>
2021-05-06 22:42:51 +0300

    Выбор активной секции

--- UI/WebStore/Components/SectionsViewComponent.cs
+++ UI/WebStore/Components/SectionsViewComponent.cs
@@ -6,53 +6,75 @@ using System.Threading.Tasks;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.ViewModels;
 
+
 namespace WebStore.Components
 {
-    //[ViewComponent(Name ="Название")]
+    //[ViewComponent(Name = "Название")]
     public class SectionsViewComponent : ViewComponent
     {
-        private IProductData _ProductData;
+        private readonly IProductData _ProductData;
 
         public SectionsViewComponent(IProductData ProductData) => _ProductData = ProductData;
 
-
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(string SectionId)
         {
-            var sections = _ProductData.GetSections();
-            var parent_sections = sections.Where(s => s.ParentId is null);
-            var parent_sections_views = parent_sections
-                .Select(s => new SectionViewModel
-                {
-                    Id = s.Id,
-                    Name = s.Name,
-                    Order = s.Order,
-                    ProductCount = s.ProductCount,
-                }
-                    ).ToList();
+            var section_id = int.TryParse(SectionId, out var id) ? id : (int?)null;
 
-            int OrderSortMethod(SectionViewModel a, SectionViewModel b) => Comparer<int>.Default.Compare(a.Order, b.Order);
+            var sections = GetSections(section_id, out var parent_section_id);
+
+            ViewBag.SectionId = section_id;
+            //ViewBag.ParentSectionId = parent_section_id;
+            ViewData["ParentSectionId"] = parent_section_id;
+
+            return View(new SelectableSectionsViewModel()
+            {
+                Sections = sections,
+                SectionId = section_id,
+                ParentSectionId = parent_section_id
+            });
+        }
+
+        private IEnumerable<SectionViewModel> GetSections(int? SectionId, out int? ParentSectionId)
+        {
+            ParentSectionId = null;
+
+            var sections = _ProductData.GetSections();
+
+            var parent_sections = sections.Where(s => s.ParentId is null);
+
+            var parent_sections_views = parent_sections
+               .Select(s => new SectionViewModel
+               {
+                   Id = s.Id,
+                   Name = s.Name,
+                   Order = s.Order
+               })
+               .ToList();
 
             foreach (var parent_section in parent_sections_views)
             {
-                var childs = sections.Where(s => s.ParentId == parent_section.Id);
-                foreach (var child_section in childs) 
+                var child = sections.Where(s => s.ParentId == parent_section.Id);
+
+                foreach (var child_section in child)
+                {
+                    if (child_section.Id == SectionId)
+                        ParentSectionId = child_section.ParentId;
+
                     parent_section.ChildsSection.Add(new SectionViewModel
                     {
                         Id = child_section.Id,
                         Name = child_section.Name,
                         Order = child_section.Order,
-                        Parent = parent_section,
-                        ProductCount = child_section.ProductCount,
-
+                        Parent = parent_section
                     });
+                }
 
-                    parent_section.ChildsSection.Sort(OrderSortMethod);
+                parent_section.ChildsSection.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
             }
 
-            parent_sections_views.Sort(OrderSortMethod);
-            return View(parent_sections_views);
-        }
+            parent_sections_views.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
 
-        //public async Task<IViewComponentResult> InvokeAsync() => View();
+            return parent_sections_views;
+        }
     }
 }
---
+++ UI/WebStore/ViewModels/SelectableSectionsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WebStore.ViewModels
+{
+    public class SelectableSectionsViewModel
+    {
+        public IEnumerable<SectionViewModel> Sections { get; set; }
+
+        public int? SectionId { get; set; }
+
+        public int? ParentSectionId { get; set; }
+    }
+}
