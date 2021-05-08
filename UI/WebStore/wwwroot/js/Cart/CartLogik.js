fedcb4afe85eef29267495cc5227adb17ea0a178
IVfromBY <ivfromby@gmail.com>
2021-05-08 17:34:58 +0300

    Подготовка разметки для подключения скрипта

---
+++ UI/WebStore/wwwroot/js/Cart/CartLogik.js
@@ -0,0 +1,15 @@
+﻿Cart = {
+    _properties: {
+        getCartViewLink: "",
+        AddToCartLink: "",
+        decrementLink: "",
+        removeFromCartLink: "",
+
+    },
+
+    init: function (properties) {
+        $.extend(Cart._properties, properties);
+
+
+    }
+}
